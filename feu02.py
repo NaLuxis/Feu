7b90b820b65c9c2c4261042ab6b245d8ec1e46c9
Yoann <yoannalves7@gmail.com>
2023-08-03 11:24:18 +0200

    first reflexion

--- feu02.py
+++ feu02.py
@@ -1,18 +1,46 @@
 ############### Trouver une forme ###############
 
+import sys
 
+from typing import List
+from utile import incorrect_argument_count
 
 ### Function ###
 
+def create_board_coords():
+    ...
 
+
+def create_shape_coords():
+    ...
+
+
+def is_equal_char(board, shape) -> List[int]:
+    ...
+
+
+def draw_shape(board_char_coords: List[int]):
+    ...
+
+
+def display_result():
+    ...
+
+
+def handle_error() -> None:
+
+    incorrect_argument_count(sys.argv, ">=", 3)
+
+    for element in sys.argv[1:]:
+        ... # regex pour savoir si tout mes arguments sont bien des fichier .txt
 
 ### Error ###
 
-
+handle_error()
 
 ### Parsing ###
 
-
+file_name = sys.argv[1]
 
 ### Problem solving ###
 
@@ -20,3 +48,4 @@
 
 ### Result ###
 
+print()
