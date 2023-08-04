d3311b5071eefa851692420a10428d8cdfcf3285
Yoann <yoannalves7@gmail.com>
2023-08-04 09:21:38 +0200

    create all .txt + advancement

--- feu02.py
+++ feu02.py
@@ -7,12 +7,34 @@ from utile import incorrect_argument_count
 
 ### Function ###
 
-def create_board_coords():
-    ...
+def read_file(file_name: str) -> List[List[str]]:
+    
+    my_file = f"/home/yoann/Documents/Feu/text/{file_name}"
 
+    with open(my_file, "r") as f:
+        content = f.readlines()
 
-def create_shape_coords():
-    ...
+        if not content:
+            print("Erreur. The file is empty.")
+            exit()
+
+        board = []
+
+        for lines in content:
+            board.append([lines[:-1]])
+        
+        return board
+    
+
+def create_coords(element: List[List[str]]) -> List[List[int]]:
+    
+    coords_board = []
+    
+    for y_axis, line in enumerate(element):
+        for x_axis, char in enumerate(line[0]):
+            coords_board.append([x_axis, y_axis])
+
+    return coords_board
 
 
 def is_equal_char(board, shape) -> List[int]:
@@ -29,23 +51,27 @@ def display_result():
 
 def handle_error() -> None:
 
-    incorrect_argument_count(sys.argv, ">=", 3)
+    incorrect_argument_count(sys.argv, "!=", 3)
 
     for element in sys.argv[1:]:
         ... # regex pour savoir si tout mes arguments sont bien des fichier .txt ( \.(txt?)$ ) 
 
 ### Error ###
 
-handle_error()
+#handle_error()
 
 ### Parsing ###
 
-file_name = sys.argv[1]
+# file_board = sys.argv[1]
+
+# file_shape = sys.argv[2]
 
 ### Problem solving ###
 
+readed_file = read_file("board.txt")
 
+element_with_coords = create_coords(readed_file)
 
 ### Result ###
 
-print()
+print(element_with_coords)
