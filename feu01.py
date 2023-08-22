8d4affaf63d2f6412e9abe9bc81f2333bbeab61a
Yoann <yoannalves7@gmail.com>
2023-08-22 13:45:45 +0200

    reset for pleasure

--- feu01.py
+++ feu01.py
@@ -1,89 +1,22 @@
 ############### Évaluer une expression ###############
 
-import sys
 
-from typing import List
-from utile import incorrect_argument_count, operation, is_string
 
 ### Function ###
 
-def clean_space(expression_to_clean: str) -> str:
-
-    expression_to_clean = expression_to_clean.replace("(", "( ")
-    expression_to_clean = expression_to_clean.replace(")", " )")
-    split_expression = expression_to_clean.split(" ")
-
-    new_list = []
-
-    for element in split_expression:
-        new_list.append(element)
-    
-    return new_list
-
-
-def calcul_operation_priority(elements_list: List[str]) -> str:
-
-    priority_operator = ("*", "/", "%")
-    non_priority_operator = ("+", "-")
-    
-    index = 0
-    while index < len(elements_list):
-        if elements_list[index] in priority_operator:
-            res = operation(float(elements_list[index - 1]), elements_list[index], float(elements_list[index + 1]))
-            elements_list[index - 1:index + 2] = [str(res)]
-        else:
-            index += 1
-
-    index = 0
-    while index < len(elements_list):
-        if elements_list[index] in non_priority_operator:
-            res = operation(float(elements_list[index - 1]), elements_list[index], float(elements_list[index + 1]))
-            elements_list[index - 1:index + 2] = [str(res)]
-        else:
-            index += 1
-
-    return elements_list[0]
-
-
-def calculate_arithmetic_expression(expression_clean: List[str]) -> str:
-    
-    stack = [[]]
-
-    for element in expression_clean:
-        if element == "(":
-            stack.append([])
-        elif element == ")":
-            sub_expr = stack.pop()
-            sub_result = calcul_operation_priority(sub_expr)
-            stack[-1].append(sub_result)
-        else:
-            stack[-1].append(element)
-  
-    return calcul_operation_priority(stack[0]) 
-
-
-def handle_error() -> None:
-
-    incorrect_argument_count(sys.argv, "!=", 2)
-
-    if not is_string(sys.argv[1]):
-        exit("Error. Argument must be string")
 
 
 ### Error ###
 
-handle_error()
+
 
 ### Parsing ###
 
-expression = sys.argv[1]
+
 
 ### Problem solving ###
 
-expression_without_space = clean_space(expression)
 
-result = calculate_arithmetic_expression(expression_without_space)
 
 ### Result ###
 
-print(result)
--- feu02.py
+++ feu02.py
@@ -1,89 +1,22 @@
 ############### Trouver une forme ###############
 
-import sys
 
-from typing import List, Tuple
-from utile import incorrect_argument_count
 
 ### Function ###
 
-def read_file(file_name: str) -> List[List[str]]:
-    
-    my_file = f"text/{file_name}"
 
-    with open(my_file, "r") as f:
-        content = f.readlines()
-
-        if not content:
-            exit("Erreur. The file is empty.")
-
-        element = []
-
-        for lines in content:
-            element.append([lines[:-1]])
-        
-        return element
-    
-
-def create_coords(element: List[List[str]]) -> List[Tuple[List[int],str]]:
-    
-    coords_and_value_element= []
-    first_line = element[0][0]
-    space = len(first_line) - len(first_line.lstrip(" "))
-    
-    for y_axis, line in enumerate(element):
-        for x_axis, char in enumerate(line[0]):
-            coords_and_value_element.append([[x_axis - space, y_axis], char])
-
-    return coords_and_value_element
-
-
-def find_equal_shape(shape_file_name: str) -> List[int]:
-    
-    board_coords_list = create_coords(read_file("board.txt"))
-    shape_coords_list = create_coords(read_file(shape_file_name))
-
-    for coords_and_value in shape_coords_list:
-        if coords_and_value[0] == [0,0]:
-            shape_char = coords_and_value[1]
-
-    for coords_and_value in board_coords_list:
-        if coords_and_value[1] == shape_char:
-            board_char = coords_and_value[0]
-
-    
-
-
-
-def display_result():
-    ...
-
-
-def handle_error() -> None:
-
-    incorrect_argument_count(sys.argv, "!=", 3)
-
-    for element in sys.argv[1:]:
-        ... # regex pour savoir si tout mes arguments sont bien des fichier .txt ( \.(txt?)$ ) 
 
 ### Error ###
 
-#handle_error()
+
 
 ### Parsing ###
 
-# file_board = sys.argv[1]
 
-# file_shape = sys.argv[2]
 
 ### Problem solving ###
 
-# readed_file = read_file("board.txt")
 
-# element_with_coords = create_coords(readed_file)
-
-shape_is_equal_or_not = find_equal_shape("to_find.txt")
 
 ### Result ###
 
-print(shape_is_equal_or_not)
